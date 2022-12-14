a6fd85c73fecbf74d02a2cceba125ebafac62d48
Bellroute <kjkun7631@gmail.com>
2022-12-14 21:27:34 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/nolnol/useminserver/UseminserverApplication.java
@@ -0,0 +1,13 @@
+package com.nolnol.useminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseminserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UseminserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nolnol/useminserver/UseminserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nolnol.useminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseminserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
